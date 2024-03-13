2a4e7a05fd487e01711eda9e46aacbacebac2b49
MattBixley <matt@Horace>
2024-03-14 11:04:11 +1300

    matrix

--- scripts/matrix.R
+++ scripts/matrix.R
@@ -6,22 +6,16 @@ n <-  5000
 A <- matrix (runif (m*n),m,n)
 
 # Matrix multiply
-system.time (B <- crossprod(A))
+MM <- system.time (B <- crossprod(A))
 
 # Cholesky Factorization
-system.time (C <- chol(B))
+Chol <- system.time (C <- chol(B))
 
 # Singular Value Deomposition
-m <- 20000
-n <- 2000
-A <- matrix (runif (m*n),m,n)
-system.time (S <- svd (A,nu=0,nv=0))
+SVD <- system.time (S <- svd (A,nu=0,nv=0))
 
 # Principal Components Analysis
-m <- 20000
-n <- 2000
-A <- matrix (runif (m*n),m,n)
-system.time (P <- prcomp(A))
+PCA <- system.time (P <- prcomp(A))
 
 # Linear Discriminant Analysis
 library('MASS')
@@ -29,4 +23,11 @@ g <- 5
 k <- round (m/2)
 A <- data.frame (A, fac=sample (LETTERS[1:g],m,replace=TRUE))
 train <- sample(1:m, k)
-system.time (L <- lda(fac ~., data=A, prior=rep(1,g)/g, subset=train))
+LDA <- system.time (L <- lda(fac ~., data=A, prior=rep(1,g)/g, subset=train))
+
+output <- rbind(MM, Chol, SVD, PCA, LDA)
+# get the number of cpus
+cpus <- as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK"))
+
+write.table(output, file =paste0("time_",cpus,".txt"), quote = F, sep=",")
+
--- scripts/testr.sl
+++ scripts/testr.sl
@@ -1,11 +1,29 @@
 #!/bin/bash -e
+#SBATCH --job-name=SpeedTest_R
 #SBATCH --time=00:20:00
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=3G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=10G
 #SBATCH --account=2023-nesi_slurm_testing
-#SBATCH --partition=interactive
+#SBATCH --partition=compute
 
+## clean working environment and load required modules
 module purge
 module load R/4.3.2-foss-2023a
 
-Rscript matrix.R
+## make tmp dir for outputs
+dir=/agr/scratch/projects/2023-nesi_slurm_testing/${SLURM_JOB_ID}
+echo $dir
+mkdir -p $dir
+cd $dir
+
+submitdir=${SLURM_SUBMIT_DIR}
+echo $submitdir
+
+## run actual job
+Rscript $submitdir/matrix.R
+
+
+## copy and clean up
+cp $dir/* $submitdir
+rm -r $dir
+
